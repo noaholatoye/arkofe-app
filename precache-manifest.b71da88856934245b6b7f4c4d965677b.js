5ecc1678552b55548e7535bd344d28ace0597e1d
Noah Olatoye <noaholatoye@gmail.com>
2019-07-31 04:42:27 +0100

    Updates

---
+++ precache-manifest.b71da88856934245b6b7f4c4d965677b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81f996b396e0962d55589804c19c1e7a",
+    "url": "/arkofe-app/index.html"
+  },
+  {
+    "revision": "7581b5722cf9de8f08e4",
+    "url": "/arkofe-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/arkofe-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "7581b5722cf9de8f08e4",
+    "url": "/arkofe-app/static/js/main.702366b9.chunk.js"
+  },
+  {
+    "revision": "53f876befe0c5b0cc608",
+    "url": "/arkofe-app/static/js/runtime~main.15ba3ea2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/arkofe-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
